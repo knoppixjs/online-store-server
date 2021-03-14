1c8a95e312e28f2bf11dcb9916b163d440605866
Константин Б. Фатеев <Fateyev@isate.kz>
2021-03-14 19:09:33 +0600

    init

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fileUpload = require('express-fileupload')
 const router = require('./routes')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
-const PORT = 5000
+const PORT = 4000
 
 const app = express()
 app.use(cors())
